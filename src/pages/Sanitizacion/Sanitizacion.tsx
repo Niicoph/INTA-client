fc312b20354f9ef4649511990528bbbd3b4abeea
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2025-06-16 04:57:47 -0300

    feat: un poco más responsive

--- src/pages/Sanitizacion/Sanitizacion.tsx
+++ src/pages/Sanitizacion/Sanitizacion.tsx
@@ -7,19 +7,19 @@ import VisualizacionSanitizacion from '@/components/Containers/VisualizacionSani
 export default function Sanitizacion() {
   return (
     <section className="grid grid-cols-1 gap-4 w-full h-full xl:flex">
-      <div className="flex flex-row gap-4 h-full w-full xl:w-3/5">
+      <div className="flex flex-col 2xl:flex-row gap-4 h-full w-full 2xl:w-1/2">
         <ProductosProvider>
-            <div className='grid grid-cols-1 gap-4 w-1/3'>
+            <div className='flex flex-col gap-4 2xl:w-2/5'>
               <ContainerCotizaciones dollar={true}/>
               <FormProductos />
             </div>
-            <div className='grid grid-cols-1 gap-4 w-2/3'>
+            <div className='grid grid-cols-1 gap-4 2xl:w-3/5'>
               <FormPlan />
             </div>
           
         </ProductosProvider>
       </div>
-      <div className="w-full xl:flex xl:flex-row xl:w-2/5">
+      <div className="w-full 2xl:flex 2xl:flex-row 2xl:w-1/2">
         <VisualizacionSanitizacion />
       </div>
     </section>
